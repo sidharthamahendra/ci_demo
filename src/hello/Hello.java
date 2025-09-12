312e7f4dcf26db21ab6d713812e6bf4d3a0f8380
sidharthamahendra <151663779+sidharthamahendra@users.noreply.github.com>
2025-09-12 14:23:04 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
 public static void main(String[] args) {
 System.out.println("Hello from Pipeline CI!");
 System.out.println("Hello from ngirok!");
-System.out.println("Hello from it-1!");
+System.out.println("Hello from ngok agent!");
 }
 }
